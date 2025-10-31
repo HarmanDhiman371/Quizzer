5c30ecfefd0d7010d8abf071155acd81417323a0
Harman Dhiman <harmandhiman6776@gmail.com>
2025-10-31 19:30:12 +0530

    changing db and added fresh new db

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,15 +1,18 @@
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCq--mTlPsYGN2F59pf1hACigEsq9V4Cmk",
-  authDomain: "first-a7079.firebaseapp.com",
-  projectId: "first-a7079",
-  storageBucket: "first-a7079.firebasestorage.app",
-  messagingSenderId: "592843008641",
-  appId: "1:592843008641:web:3ac19c83dcdce4d0cabb80",
-  measurementId: "G-DENH5RC0EN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
